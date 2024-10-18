acb5fcd44f703afa2d0dc1d7229cef4057c35c6c
ilyan <ilyanbendib@gmail.com>
2024-10-18 19:02:34 +0200

    CPP

--- CPP-Module_06/ex02/srcs/Base.cpp
+++ CPP-Module_06/ex02/srcs/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 17:44:45 by ilbendib          #+#    #+#             */
-/*   Updated: 2024/10/18 18:12:57 by ilbendib         ###   ########.fr       */
+/*   Updated: 2024/10/18 18:20:08 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,23 @@ void identify(Base* p)
 
 void identify(Base& p)
 {
-	try {
-		A& aRef = dynamic_cast<A&>(p); // Essaie de convertir p en une référence de type A
-		std::cout << "A" << std::endl;  // Si ça réussit, on est sûr que p est de type A
-		(void)aRef; // Éviter l'avertissement sur aRef non utilisé
-	} catch (std::bad_cast&) {
-		// Si la conversion échoue, on passe à la prochaine vérification
+	Base &test = p;
+	try
+	{
+		test = dynamic_cast<A&>(p);
+		std::cout << "le type de p est A" << std::endl;
 	}
-
-	try {
-		B& bRef = dynamic_cast<B&>(p); // Essaie de convertir p en une référence de type B
-		std::cout << "B" << std::endl;  // Si ça réussit, on est sûr que p est de type B
-		(void)bRef; // Éviter l'avertissement sur bRef non utilisé
-	} catch (std::bad_cast&) {
-		// Si la conversion échoue, on passe à la prochaine vérification
+	catch(std::exception & e){}
+	try
+	{
+		test = dynamic_cast<B&>(p);
+		std::cout << "le type de p est B" << std::endl;
 	}
-
-	try {
-		C& cRef = dynamic_cast<C&>(p); // Essaie de convertir p en une référence de type C
-		std::cout << "C" << std::endl;  // Si ça réussit, on est sûr que p est de type C
-		(void)cRef; // Éviter l'avertissement sur cRef non utilisé
-	} catch (std::bad_cast&) {
-		// Si la conversion échoue, p n'est pas de type A, B ou C
-		std::cout << "Type inconnu" << std::endl;
+	catch(std::exception & e){}
+	try
+	{
+		test = dynamic_cast<C&>(p);
+		std::cout << "le type de p est C" << std::endl;
 	}
+	catch(std::exception & e){}
 }
